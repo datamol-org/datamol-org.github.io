4dceaa30463265cfd66aa199c6ef971845de9942
michelml <michmoreau.l@gmail.com>
2021-02-27 14:18:34 -0500

    complete mobile nav and update readme

--- README.md
+++ README.md
@@ -6,6 +6,34 @@ Pure static HTML/CSS website that uses: [jQuery](https://jquery.com/), [Bootstra
 
 Use [prettier](https://github.com/prettier/prettier).
 
+Install with
+
+```bash
+npm i -g prettier
+```
+
+Format with
+
+```bash
+prettier -w .
+```
+
+## Editing SASS files
+
+Use the SASS cli.
+
+Install with
+
+```bash
+npm i -g sass
+```
+
+Format with
+
+```bash
+sass scss/<name of the file>.scss css/<name of the file>.css
+```
+
 ## License
 
 This website is licensed under a [Creative Commons Attribution-ShareAlike 4.0 International License](https://creativecommons.org/licenses/by-sa/4.0/).
--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,56 @@ $(function () {
   feather.replace();
 });
 
+// tooltips
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
+});
+
+// mobile nav business logic
+var body = document.querySelector("body");
+var nav = document.querySelector("#nav");
+var waves = document.querySelector(".box");
+var menu = document.querySelector("#menu");
+var menuToggle = document.querySelector(".nav__toggle");
+var isMenuOpen = false;
+
+menuToggle.addEventListener("click", (e) => {
+  e.preventDefault();
+  isMenuOpen = !isMenuOpen;
+
+  // toggle a11y attributes and active class
+  menuToggle.setAttribute("aria-expanded", String(isMenuOpen));
+  menu.hidden = !isMenuOpen;
+  nav.classList.toggle("nav--open");
+  waves.classList.toggle("nav--open");
+
+  if (menu.hidden) {
+    bodyScrollLock.enableBodyScroll(nav);
+  } else {
+    debugger;
+    bodyScrollLock.disableBodyScroll(nav);
+  }
+});
+
+// TRAP TAB INSIDE NAV WHEN OPEN
+nav.addEventListener("keydown", (e) => {
+  // abort if menu isn't open or modifier keys are pressed
+  if (!isMenuOpen || e.ctrlKey || e.metaKey || e.altKey) {
+    return;
+  }
+
+  // listen for tab press and move focus
+  // if we're on either end of the navigation
+  var menuLinks = menu.querySelectorAll(".nav__link");
+  if (e.keyCode === 9) {
+    if (e.shiftKey) {
+      if (document.activeElement === menuLinks[0]) {
+        menuToggle.focus();
+        e.preventDefault();
+      }
+    } else if (document.activeElement === menuToggle) {
+      menuLinks[0].focus();
+      e.preventDefault();
+    }
+  }
 });
